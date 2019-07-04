21fa382516bf3d19c7eb58cbf06eac47863fc23e
Nivram3 <marvinyan3@gmail.com>
2019-07-04 11:33:27 -0400

    Add Pawn Brotherhood.py

--- Pawn Brotherhood.py
+++ Pawn Brotherhood.py
@@ -48,7 +48,7 @@ print(safe_pawns({"b4", "c4", "d4", "e4", "f4", "g4", "e5"})) #1
 
 
 '''
-Interesting alt solutions on Checkio
+Interesting altenate solutions on Checkio
 ___________________________________________________________________________________________________________________
 def safe_pawns(pawns: set) -> int:
     
